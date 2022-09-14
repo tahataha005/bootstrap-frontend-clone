9d2d594adf2a503ee2dbc01179c8d2cf95490616
tahataha005 <taha.f.taha005@gmail.com>
2022-09-14 11:34:37 +0300

    CORS

--- backend/add_message.php
+++ backend/add_message.php
@@ -1,5 +1,10 @@
 <?php
 
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: POST, GET, OPTIONS, PUT, DELETE');
+header('Access-Control-Allow-Headers: Origin, Content-Type, Accept, Authorization, X-Request-With');
+
+
 include("connection.php");
 
 $full_name = $_POST["full_name"];
--- backend/read_messages.php
+++ backend/read_messages.php
@@ -1,5 +1,10 @@
 <?php
 
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: POST, GET, OPTIONS, PUT, DELETE');
+header('Access-Control-Allow-Headers: Origin, Content-Type, Accept, Authorization, X-Request-With');
+
+
 include("connection.php");
 
 $q = $sqli->prepare("SELECT full_name,message FROM messages");
